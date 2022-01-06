375ee97090037a9a257612782a3f8771ce352f9c
houdini217 <koya2170@gmail.com>
2022-01-06 18:15:43 +0900

    Separate codes in main into another methods

--- JackAnalyzer/main.swift
+++ JackAnalyzer/main.swift
@@ -17,45 +17,52 @@ class JackAnalyzer {
         guard arguments.count > 1 else {
             fatalError("target .vm file is not specified.")
         }
-        
-        //        let fileUrl = currentPath + "/" + arguments[1]
+
         let url = URL(fileURLWithPath: arguments[1])
         if !url.isDirectory {
-            let fileName = url.lastPathComponent
-            
-            guard fileName.hasSuffix(".jack") else {
-                fatalError("Selected file is not .jack format")
-            }
-            
-            let outputFile = fileName.replacingOccurrences(of: "jack", with: "xml")
-            let outputFileDir = url.deletingLastPathComponent().appendingPathComponent(outputFile)
-            let tokenizer = JackTokenizer(fileURL: url)
+            Self.compileSingleFile(url)
         } else {
-            do {
-                // Get the directory contents urls (including subfolders urls)
-                let directoryContents =
-                    try FileManager
-                        .default
-                        .contentsOfDirectory(
-                            at: url,
-                            includingPropertiesForKeys: nil
-                )
-                let jackFileUrls = directoryContents.filter{ $0.pathExtension == "jack" }
-                let outputFile = url.lastPathComponent.appending(".xml")
-                
-                guard jackFileUrls.count != 0 else {
-                    fatalError("There has not contained .jack format file")
-                }
-                
-                let outputFileDir = jackFileUrls.first!.deletingLastPathComponent().appendingPathComponent(outputFile)
-                
-                for jackUrl in jackFileUrls {
+            self.compileMultiFiles(url)
+        }
+    }
 
-                }
-                
-            } catch {
-                fatalError("Unable to get list of files inside the directory")
+    private static func compileSingleFile(_ url: URL) {
+        let fileName = url.lastPathComponent
+        
+        guard fileName.hasSuffix(".jack") else {
+            fatalError("Selected file is not .jack format")
+        }
+        
+        let outputFile = fileName.replacingOccurrences(of: "jack", with: "xml")
+        let outputFileDir = url.deletingLastPathComponent().appendingPathComponent(outputFile)
+        let tokenizer = JackTokenizer(fileURL: url)
+    }
+
+    private static func compileMultiFiles(_ url: URL) {
+        do {
+            // Get the directory contents urls (including subfolders urls)
+            let directoryContents =
+                try FileManager
+                    .default
+                    .contentsOfDirectory(
+                        at: url,
+                        includingPropertiesForKeys: nil
+            )
+            let jackFileUrls = directoryContents.filter{ $0.pathExtension == "jack" }
+            let outputFile = url.lastPathComponent.appending(".xml")
+            
+            guard jackFileUrls.count != 0 else {
+                fatalError("There has not contained .jack format file")
             }
+            
+            let outputFileDir = jackFileUrls.first!.deletingLastPathComponent().appendingPathComponent(outputFile)
+            
+            for jackUrl in jackFileUrls {
+
+            }
+            
+        } catch {
+            fatalError("Unable to get list of files inside the directory")
         }
     }
 }
