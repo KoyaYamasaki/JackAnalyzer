f4bea519ae491d11133b4d7694f7ae1ca2ff5b49
houdini217 <koya2170@gmail.com>
2022-01-10 11:57:21 +0900

    Make some progress

--- JackAnalyzer/JackTokenizer.swift
+++ JackAnalyzer/JackTokenizer.swift
@@ -10,7 +10,12 @@ import Foundation
 
 class JackTokenizer {
 
-    private var commandByLine: [String] = []
+    var currentToken = ""
+
+    private let preservedDelimiters: Set<Character> =
+        ["{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/",
+    "&", "|", "<", ">", "=", "~"]
+    private var position: Int = -1
     private var tokenList: [String] = []
 
     init(fileURL: URL) {
@@ -19,7 +24,7 @@ class JackTokenizer {
             fatalError("File could not be read.")
         }
 
-        commandByLine = fileContents.components(separatedBy: "\n")
+        var commandByLine = fileContents.components(separatedBy: "\n")
 
         // Remove comment lines.
         commandByLine = commandByLine.filter {
@@ -39,11 +44,100 @@ class JackTokenizer {
         // Remove line break lines.
         commandByLine = commandByLine.filter({ $0 != "\r" && !$0.isEmpty })
 
+        commandByLine = commandByLine.map {
+            if $0.contains("\"") {
+                let firstIndex = $0.firstIndex(of: "\"")!
+                let lastIndex = $0.lastIndex(of: "\"")!
+                let lIndex = $0.index(lastIndex, offsetBy: 1)
+                let strVal = $0[firstIndex...lastIndex]
+                return String($0[..<firstIndex]) + String(strVal).replacingOccurrences(of: " ", with: "") + String($0[lIndex...])
+            } else {
+                return $0
+            }
+        }
+
         for aOrder in commandByLine {
             let tokens = aOrder.components(separatedBy: " ")
-            _ = tokens.map { tokenList.append($0) }
+
+            _ = tokens.map { token in
+                var shapedToken = ""
+                token.forEach { char in
+                    if preservedDelimiters.contains(char) {
+                        if !shapedToken.isEmpty {
+                            tokenList.append(shapedToken)
+                            shapedToken = ""
+                        }
+                        tokenList.append(String(char))
+                    } else {
+                        shapedToken.append(char)
+                    }
+                }
+                if !shapedToken.isEmpty {
+                    tokenList.append(shapedToken)
+                }
+            }
         }
 
         print(tokenList)
+    }
+
+    var tokenType: TokenType {
+        if Int(currentToken) != nil {
+            return .INT_CONST
+        }
+
+        if currentToken.contains("\"") {
+            return .STRING_CONST
+        }
+
+        switch currentToken {
+        case "class", "constructor", "function", "method", "field", "static",
+             "var", "int", "char", "boolean", "void", "true", "false", "null",
+             "this", "let", "do", "if", "else", "while", "return":
+            return .KEYWORD
+        case "{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/",
+             "&", "|", "<", ">", "=", "~":
+            return .SYMBOL
+        default:
+            return .IDENTIFIER
+        }
+    }
+
+    func hasMoreCommands() -> Bool {
+        return position + 1 != tokenList.count
+    }
+
+    func advance() {
+        if hasMoreCommands() {
+            position += 1
+            currentToken = tokenList[position]
+        }
+    }
+
+    func keyword() -> String {
+        return currentToken
+    }
+
+    func symbol() -> String {
+        return currentToken
+    }
+
+    func identifier() -> String {
+        return currentToken
+    }
+
+    func intVal() -> Int {
+        if let intToken = Int(currentToken) {
+            return intToken
+        } else {
+            fatalError("currentToken is not int value.")
+        }
+    }
+
+    func stringVal() -> String {
+        if currentToken.contains("\"") {
+        }
+
+        return ""
     }
 }
